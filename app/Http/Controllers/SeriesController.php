e1588cb9991340857c233721e851982976d3b6e5
Rodrigo Cabral <rodrigoalcab@gmail.com>
2019-12-04 21:37:47 +0000

    criado o método show por ID

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -5,11 +5,33 @@ namespace App\Http\Controllers;
 
 
 use App\Serie;
+use Illuminate\Http\Request;
 
 class SeriesController
 {
     public function index()
     {
         return Serie::all();
+    }
+
+    public function store(Request $request)
+    {
+
+        return response()
+            ->json(
+                Serie::create(['nome' => $request->nome]),
+                201);
+    }
+
+    public function show(int $id)
+    {
+        $serie = Serie::find($id);
+
+        if (is_null($serie)) {
+            return response()->json('', 204);
+        }
+
+        return response()->json($serie);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => '/api'], function () use ($router){
-    $router->get('/series', 'SeriesController@index');
+$router->group(['prefix' => 'api'], function () use ($router){
+    $router->get('series', 'SeriesController@index');
+    $router->post('series', 'SeriesController@store');
+    $router->get('series/{id}', 'SeriesController@show');
 });
 
 
